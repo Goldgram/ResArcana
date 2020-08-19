502c2619651ab4c17ad4c2238b0cb70b9739ff17
Goldgram <willbrennandesign@gmail.com>
2020-08-19 01:46:06 +0100

    add most magic items

---
+++ res-arcana/src/app/poc/consts/magic-items.ts
@@ -0,0 +1,115 @@
+import { ItemInfo } from '../types';
+
+const CALM_OR_ELAN: ItemInfo = {
+  id: 'magic-item-1',
+  title: 'Calm / Elan',
+  type: 'magic-item',
+  collect: [{ blue: 1 }, { red: 1 }],
+};
+
+const DEATH_OR_LIFE: ItemInfo = {
+  id: 'magic-item-2',
+  title: 'Death / Life',
+  type: 'magic-item',
+  collect: [{ black: 1 }, { green: 1 }],
+};
+
+const DIVINATION: ItemInfo = {
+  id: 'magic-item-3',
+  title: 'Divination',
+  type: 'magic-item',
+  actions: [
+    {
+      cost: { tap: ['self'] },
+      reward: { drawAndDiscard: [3, 3] },
+    },
+  ],
+};
+
+const ALCHEMY: ItemInfo = {
+  id: 'magic-item-4',
+  title: 'Alchemy',
+  type: 'magic-item',
+  actions: [
+    {
+      cost: { tap: ['self'], discard: { resources: { wild: 4 } } },
+      reward: {
+        gain: {
+          resources: { gold: 2 },
+          target: 'player',
+        },
+      },
+    },
+  ],
+};
+
+const TRANSMUTATION: ItemInfo = {
+  id: 'magic-item-5',
+  title: 'Transmutation',
+  type: 'magic-item',
+  actions: [
+    {
+      cost: { tap: ['self'], discard: { resources: { wild: 3 } } },
+      reward: {
+        gain: {
+          resources: {
+            wild: 3,
+            wildRestrictions: ['gold'],
+          },
+          target: 'player',
+        },
+      },
+    },
+  ],
+};
+
+const RESEARCH: ItemInfo = {
+  id: 'magic-item-6',
+  title: 'Research',
+  type: 'magic-item',
+  actions: [
+    {
+      cost: { tap: ['self'], discard: { resources: { wild: 1 } } },
+      reward: { drawCards: 1 },
+    },
+  ],
+};
+
+const PROTECTION: ItemInfo = {
+  id: 'magic-item-7',
+  title: 'Protection',
+  type: 'magic-item',
+  reactions: [
+    {
+      type: 'any-life-loss',
+      cost: { tap: ['self'] },
+      reward: { ignore: true },
+    },
+  ],
+};
+
+const REANIMATE: ItemInfo = {
+  id: 'magic-item-8',
+  title: 'Reanimate',
+  type: 'magic-item',
+  actions: [
+    {
+      cost: { tap: ['self'], discard: { resources: { wild: 1 } } },
+      reward: { untap: 'any' },
+    },
+  ],
+};
+
+// ILLUSION
+// INSCRIPTION
+
+export const MAGIC_ITEMS = [
+  CALM_OR_ELAN,
+  DEATH_OR_LIFE,
+  DIVINATION,
+  ALCHEMY,
+  TRANSMUTATION,
+  RESEARCH,
+  PROTECTION,
+  REANIMATE,
+];
--- res-arcana/src/app/poc/index.tsx
+++ res-arcana/src/app/poc/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { MAGES } from './consts/mages';
 import { MONUMENTS } from './consts/monuments';
 import { ARTIFACTS } from './consts/artifacts';
+import { MAGIC_ITEMS } from './consts/magic-items';
 
 export const POC = () => {
   return (
@@ -12,6 +13,7 @@ export const POC = () => {
       <br />
       Artifacts: {ARTIFACTS.length}
       <br />
+      Magic Items: {MAGIC_ITEMS.length}
     </>
   );
 };
